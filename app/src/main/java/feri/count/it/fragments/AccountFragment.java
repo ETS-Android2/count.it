78edfea7bd655dd589a443d699b7f453f2209e6c
jana_j <jana.jankovic@student.um.si>
2021-03-30 15:43:19 +0200

    user account xml and padding in entry

--- app/src/main/java/feri/count/it/fragments/AccountFragment.java
+++ app/src/main/java/feri/count/it/fragments/AccountFragment.java
@@ -1,14 +1,19 @@
 package feri.count.it.fragments;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import feri.count.it.R;
+import feri.count.it.activities.MenuActivity;
 
 public class AccountFragment extends Fragment {
 
@@ -20,7 +25,40 @@ public class AccountFragment extends Fragment {
 
         final ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_account, null);
+        transparentStatusAndNavigation();
 
         return rootView;
+    }
+
+    private void transparentStatusAndNavigation() {
+        //make full transparent statusBar
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            ((MenuActivity) requireActivity()).getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            );
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, false);
+            ((MenuActivity) requireActivity()).getWindow().setStatusBarColor(Color.TRANSPARENT);
+            ((MenuActivity) requireActivity()).getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    private void setWindowFlag(final int bits, boolean on) {
+        Window win = ((MenuActivity) requireActivity()).getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 }
