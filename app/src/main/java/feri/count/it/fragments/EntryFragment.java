3eda4aa4dc32c40c1e1eae173af6cec7b8c70e4c
jana_j <jana.jankovic@student.um.si>
2021-04-11 20:48:05 +0200

    designed home fragment, changed visibility of status bar

--- app/src/main/java/feri/count/it/fragments/EntryFragment.java
+++ app/src/main/java/feri/count/it/fragments/EntryFragment.java
@@ -1,5 +1,6 @@
 package feri.count.it.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import feri.count.it.R;
+import feri.count.it.activities.MenuActivity;
 
 public class EntryFragment extends Fragment {
 
@@ -21,6 +23,14 @@ public class EntryFragment extends Fragment {
 
         final ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_entry, null);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ((MenuActivity) requireActivity()).getWindow().setStatusBarColor(getResources().getColor(R.color.maximum_blue, ((MenuActivity) requireActivity()).getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ((MenuActivity) requireActivity()).getWindow().setStatusBarColor(getResources().getColor(R.color.maximum_blue));
+        }
+        ((MenuActivity) requireActivity()).getSupportActionBar().hide();
 
         return rootView;
     }
--- app/src/main/java/feri/count/it/fragments/HomeFragment.java
+++ app/src/main/java/feri/count/it/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package feri.count.it.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import feri.count.it.R;
+import feri.count.it.activities.MenuActivity;
 
 public class HomeFragment extends Fragment {
 
@@ -25,6 +27,13 @@ public class HomeFragment extends Fragment {
 
         final ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_home, null);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ((MenuActivity) requireActivity()).getWindow().setStatusBarColor(getResources().getColor(R.color.maximum_blue, ((MenuActivity) requireActivity()).getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ((MenuActivity) requireActivity()).getWindow().setStatusBarColor(getResources().getColor(R.color.maximum_blue));
+        }
+        ((MenuActivity) requireActivity()).getSupportActionBar().hide();
 
         return rootView;
     }
